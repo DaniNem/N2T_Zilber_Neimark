97eabf2d524f9c1f957794c7ac09e0424a0fd2d3
Unknown <amitsilber@gmail.com>
2017-11-20 19:36:31 +0200

    Conditions operations

---
+++ projects/07/VMtranslator/EqualOp.py
@@ -0,0 +1,13 @@
+from ConditionsOp import ConditionsOp as CO
+
+
+class EqualOp(CO):
+    """
+    a parser that convert vm == command to assembly
+    """
+
+    def __init__(self):
+        """
+        initiate operation
+        """
+        self.jump_condition = "JNE"
