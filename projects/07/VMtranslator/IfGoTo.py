77edf85838f372aeee6ea5a41ed72b05a148acba
Unknown <amitsilber@gmail.com>
2017-11-27 17:29:22 +0200

    if-goto condition

---
+++ projects/07/VMtranslator/IfGoTo.py
@@ -0,0 +1,24 @@
+class InGoTo(object):
+    IF_GOTO = "if-goto"
+
+    def set_params(self, file_name, func_name):
+        self.func_name = func_name
+        self.file_name = file_name
+
+    def parse(self, line, output_ds):
+        line_arr = line.split(" ")
+        label = self.file_name + "." + self.func_name + "$" + str(line_arr[1])
+        output_ds.append("@SP")
+        output_ds.append("M=M-1")
+        output_ds.append("A=M")
+        output_ds.append("D=M")
+        output_ds.append("@"+label)
+        output_ds.append("D;JLT")
+
+
+    def is_triggered(self, line):
+        """
+        :param line: line of vm code
+        :return: true if the line belongs to a certain memory segment
+        """
+        return self.IF_GOTO in line
