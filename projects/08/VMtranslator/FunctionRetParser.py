7b2c29d84d6b769b61057283efc9852ed13691fb
Unknown <amitsilber@gmail.com>
2017-12-04 15:21:56 +0200

    doc added 2

--- projects/08/VMtranslator/FunctionRetParser.py
+++ projects/08/VMtranslator/FunctionRetParser.py
@@ -1,21 +1,37 @@
 class FunctionRetParser(object):
+    """
+    convert function return statement to assembly
+    """
     def __init__(self):
         """
+        set initial params
         """
         self.file_name = ""
         self.ID = "return"
 
     def is_triggered(self, line):
-        '''
-
-        :return:
-        '''
+        """
+        check if line match the parser
+        :param line: given vm line
+        :return: true if matched
+        """
         return line.startswith(self.ID)
 
     def set_params(self, file_name):
+        """
+        set name param
+        :param file_name: given file name
+        :return:
+        """
         self.file_name = file_name
 
     def parse(self, line, output_ds):
+        """
+        parse vm line to assembly
+        :param line: given vm line
+        :param output_ds: assembly ds to add commands
+        :return:
+        """
         # R13 = LCL
         output_ds.append("@LCL")
         output_ds.append("D=M")
@@ -70,12 +86,8 @@ class FunctionRetParser(object):
         output_ds.append("@LCL")
         output_ds.append("M=D")
 
-
+        # jump from function
         output_ds.append("@R14")
         output_ds.append("A=M")
         output_ds.append("1;JMP")
 
-
-
-if __name__ == "__main__":
-    pass
