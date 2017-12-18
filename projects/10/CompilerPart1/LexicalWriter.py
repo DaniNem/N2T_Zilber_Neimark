78528a1b31b219831fdab970e6d7d03e06d3a68a
DaniNem <DaniNem@gmail.com>
2017-12-18 17:43:10 +0200

    fix minor bug

--- projects/10/CompilerPart1/LexicalWriter.py
+++ projects/10/CompilerPart1/LexicalWriter.py
@@ -1,4 +1,6 @@
-import xml.etree.cElementTree as ET
+import xml.etree.ElementTree as ET
+
+#import xml.dom.minidom as minidom
 
 
 class LexicalWriter(object):
@@ -8,39 +10,52 @@ class LexicalWriter(object):
         self.keywords = ['char','int','boolean','void']
 
     def write(self, token, key = ''):
-        if token in self.keywords:
-            key = "keyword"
-        else:
-            key = "identifier"
+        if key == '':
+            if token in self.keywords:
+                key = "keyword"
+            else:
+                key = "identifier"
+        token = " " + token + " "
         ET.SubElement(self.subElements[len(self.subElements)-1],
                       key).text = token
         return
 
     def openSub(self,name):
-        self.subElements.append(ET.SubElement(self.subElements//
+        self.subElements.append(ET.SubElement(self.subElements
                                               [len(self.subElements)-1],name))
     def closeSub(self):
-        self.subElements.pop();
+        self.subElements.pop()
 
     def writeXML(self,path):
         tree = ET.ElementTree(self.root)
-        #print(tree.getroot().items())
-        self.indent(self.root)
+        self.__indent(self.root)
+        #reparsed = minidom.parseString(ET.tostring(self.root))
+        #print(reparsed.toprettyxml())
+        #with open(path,'w') as f:
+        #    f.write(reparsed.toprettyxml())
         tree.write(path)
-    def indent(self,elem, level=0):
-        i = "\n" + level*"  "
+
+    '''
+    copy and paste from http://effbot.org/zone/element-lib.htm#prettyprint
+    it basically walks your tree and adds spaces and newlines so the tree is
+    printed in a nice way
+    '''
+
+    def __indent(self,elem, level=0):
+        i = "\n" + level * "  "
         if len(elem):
             if not elem.text or not elem.text.strip():
                 elem.text = i + "  "
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
             for elem in elem:
-                self.indent(elem, level+1)
+                self.__indent(elem, level + 1)
             if not elem.tail or not elem.tail.strip():
                 elem.tail = i
         else:
             if level and (not elem.tail or not elem.tail.strip()):
                 elem.tail = i
+
 
 if __name__ == "__main__":
     root = ET.Element("root")
